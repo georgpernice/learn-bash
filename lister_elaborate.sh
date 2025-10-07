5a1c7b856faa1c2d65986a8ae4a162863fdbe34a
qu1191 <qu1191@partner.kit.edu>
2025-10-07 23:02:44 +0200

    unfertig

--- lister_elaborate.sh
+++ lister_elaborate.sh
@@ -1,12 +1,14 @@
-#usr/bin/bash
-
-for item in $(ls); do { 
-  buffer=$item;
-
-  suffix_at=`expr index $item .`;
-  filetype="${item:$suffix_at:100}";
-  buffer+="| Filetype: $filetype";
-  echo $buffer
-
-}
-done;
+#!usr/bin/bash
+files="$(ls)";
+amount="${#files}"
+count=1
+echo "Will now count thru $amount files " 
+#while [ $count -le $amount ]; do {
+#  buffer=$item;
+#  suffix_at=`expr index $item .`;
+#  filetype="${item:$suffix_at:100}";
+#  buffer+="| Filetype: $filetype";
+#  echo $buffer
+#  ((count++))
+#}
+#done;
